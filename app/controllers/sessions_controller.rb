33db0939321555f1d7663abbdcdc233f70bb16b9
Denis Wambua <denswam@gmail.com>
2018-05-23 03:03:34 -0700

    init

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,33 @@
 class SessionsController < ApplicationController
 
   def new
+    if params[:signin_option]="google"
     redirect_to '/auth/google_oauth2'
+  else
+    render 'new'
   end
 
   def create
-    auth = request.env["omniauth.auth"]
+      auth = request.env["omniauth.auth"]
+    if auth
+
     user = User.where(:provider => auth['provider'],
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = user.id
     redirect_to root_url, :notice => 'Signed in!'
+  else
+    reset_session
+   @user = User.find_by(email: session_params[:email])
+
+   if @user && @user.authenticate(session_params[:password])
+     session[:user_id] = @user.id
+     flash[:success] = 'Welcome back!'
+     redirect_to root_path
+   else
+     flash[:error] = 'Invalid email/password combination'
+     redirect_to login_path
+   end
   end
 
   def destroy
@@ -20,6 +37,9 @@ class SessionsController < ApplicationController
 
   def failure
     redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  end
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user?, :except => [:index]
+  before_action :reset_session
+
+
+  def new
+     @user = User.new
+   end
+
+   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] =  'You have successfully created an account!'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   def index
     @users = User.all
@@ -10,4 +28,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
